d430667eda8975de337eb3bba60dcee905a1a9ac
sellas201105 <74633011+sellas201105@users.noreply.github.com>
2020-11-18 16:14:01 +0900

    Information added

--- web_crawler.py
+++ web_crawler.py
@@ -8,23 +8,24 @@ import json, csv
 
 class Crawler():
     def __init__(self, category='nightlord'):
-        with open('./category.json','r') as json_file:
-            self.categories = json.load(json_file).keys()
+        with open('./category.json','r', encoding='utf-8') as json_file:
+            self.categories = json.load(json_file)
         self.url = os.path.join('https://maple.gg/job/', category)
         self.html = requests.get(self.url)
 
     def crawler(self): 
         
         self.bs = BeautifulSoup(self.html.text, 'lxml')
+        self.kor_names = self.bs.select('li > a')
         
 
     def find_population(self):
         today = datetime.datetime.today()
         file_name = today.strftime('%y-%m-%d') + '_population.csv'
         pop_file = open(file_name, 'w', newline='')
-        
         csv_writer = csv.writer(pop_file)
-        for category in self.categories:
+
+        for category in self.categories.keys():
             cat_name = category.lower().replace(' ', '')
             self.__init__(category = cat_name)
             self.crawler()
@@ -33,7 +34,10 @@ class Crawler():
             for line in lines:
                 if '명' in line.text:
                     num = int(line.text[4:-1].replace(',', ''))
-                    csv_writer.writerow([cat_name, num])
+                    
+                    # import ipdb; ipdb.set_trace()
+                    cat_info = self.categories[category]
+                    csv_writer.writerow([cat_name, cat_info[0], cat_info[1], cat_info[2], num])
 
                     print('{} \t : {}'.format(cat_name, num))
 
